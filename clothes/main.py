92df4f4dd4e6160f3fcb20054eb3cd2dce7ba024
tvblackman1 <vbif.misha2012@yandex.ru>
2021-01-01 15:58:35 +0300

    new year commit!!! Happy New Year!

--- clothes/main.py
+++ clothes/main.py
@@ -8,4 +8,16 @@ from tensorflow.keras import utils
 x_train = x_train.reshape(60000, 784)
 x_train /= 255
 
+y_train = utils.to_categorical(y_train, 10)
 
+classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
+
+model = Sequential()
+
+model.add(Dense(800, input_dim=784, activation='relu'))
+model.add(Dense(100, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='SGD', metrics=['accuracy'])
+
+print(model.summary())
